51b6f74feab3f40117b1d82d56ae41a331492084
Carlos Brandt <carloshenriquebrandt@gmail.com>
2018-06-28 18:27:46 +0200

    Fix FILE reference/path bug in inotify/cron interface

--- bin/inotify_cron_interface.sh
+++ bin/inotify_cron_interface.sh
@@ -9,7 +9,8 @@ function do_inotify_job () {
   local UPLOAD_DIR="$1"
 
   FILES=(${UPLOAD_DIR}/*.tar)
-  for FILE in ${FILES[*]}; do
+  for FILE_i in ${FILES[*]}; do
+    FILE=$(basename "$FILE_i")
     "${HERE}/../bin/data_move_stage.sh" _fakesignal_ "$FILE" "$UPLOAD_DIR"
     sleep 1
   done
